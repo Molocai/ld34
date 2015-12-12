689c1ec4e3fb3717d0358ea2e99fdd1515ef32ad
Molocai <molocai@molocai.com>
2015-12-12 11:03:07 +0100

    main menu base

---
+++ Assets/Scripts/MainMenu/MainMenuHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour {
+
+    public string sceneNameToLoad;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneNameToLoad);
+    }
+}
