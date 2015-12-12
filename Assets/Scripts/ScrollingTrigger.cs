157819f2432d8b8a58ae6e04268655bb9bd7e5b3
MissCaligari <noellievelez@gmail.com>
2015-12-12 21:09:45 +0100

    Scrolling

---
+++ Assets/Scripts/ScrollingTrigger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTrigger : MonoBehaviour
+{
+    public bool left, right, activated = true;
+    public GameObject toD;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("In ff");
+        if (other.gameObject.tag == "MainCamera")
+        {
+            Debug.Log("In tr");
+            if (activated && left)
+            {
+                GameObject space = Instantiate(toD, new Vector3(toD.transform.position.x - 16.0f, toD.transform.position.y, toD.transform.position.z), Quaternion.identity) as GameObject;
+                
+                activated = false;
+            }
+            else if (activated && right)
+            {
+                GameObject space = Instantiate(toD, new Vector3(toD.transform.position.x + 16.0f, toD.transform.position.y, toD.transform.position.z), Quaternion.identity) as GameObject;
+                activated = false;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Color giz = Color.yellow;
+        giz.a = 0.5f;
+        Gizmos.color = giz;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+}
