5bf6b068c02330e1e16e6097910b1e12c26c7154
Molocai <molocai@molocai.com>
2015-12-12 17:54:55 +0100

    zob

--- Assets/Scripts/Controller/Tmp.cs
+++ Assets/Scripts/Controller/Tmp.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tmp : MonoBehaviour {
+public class Tmp : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Zob")
+            Debug.Log("Touché!");
+    }
 }
