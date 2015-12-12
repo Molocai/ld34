1acad2d9e2ef1444d8cbc4715a49700e5cd26352
Molocai <molocai@molocai.com>
2015-12-12 19:20:43 +0100

    UI

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     // Singleton
     #region Singleton
@@ -17,6 +18,12 @@ public class PlayerController : MonoBehaviour {
     }
     #endregion
 
+    // Events
+    #region Events
+    public delegate void PlayerGetHit(int life, int max);
+    public static event PlayerGetHit OnPlayerGetHit;
+    #endregion
+
     private Rigidbody2D body;
 
     [Header("Propulseurs")]
@@ -26,12 +33,19 @@ public class PlayerController : MonoBehaviour {
     [Header("Variables")]
     public float thrusterForce = 1.0f;
 
+    [Range(0, 10f)]
+    public int spaceshipMaxHealth = 10;
+    [Range(0, 10f)]
+    public int spaceshipHealth = 10;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         body = GetComponent<Rigidbody2D>();
-	}
-	
-	void FixedUpdate () {
+    }
+
+    void FixedUpdate()
+    {
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -41,6 +55,31 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey(KeyCode.DownArrow))
         {
             body.AddForceAtPosition(transform.TransformDirection(1, -0.5f, 0) * thrusterForce, topThruster.transform.position);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        if (spaceshipHealth - 1 == 0)
+        {
+            // Die
+        }
+
+        else
+        {
+            spaceshipHealth--;
+
+            // Fire event
+            if (OnPlayerGetHit != null)
+                OnPlayerGetHit(spaceshipHealth, spaceshipMaxHealth);
         }
     }
 }
