d06b514b8e3cf5ae52ddec7f55dfd84dd1f10a62
Molocai <molocai@molocai.com>
2015-12-13 18:32:27 +0100

    Test cinematic et shake

---
+++ Assets/Cinematic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cinematic : MonoBehaviour {
+public AudioClip cinematic;
+public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+
+
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/cameraShake.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+public class cameraShake : MonoBehaviour
+{
+    private Vector3 originPosition;
+    private Quaternion originRotation;
+    public float shake_decay;
+    public float shake_intensity;
+    private Transform _transform;
+    /*void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Shake"))
+        {
+            Shake();
+        }
+    }*/
+
+       
+
+    void Update()
+    {
+
+        Shake();
+
+        if (shake_intensity > 0)
+        {
+            transform.localPosition = originPosition + Random.insideUnitSphere * shake_intensity;
+           /* transform.rotation = new Quaternion(
+            originRotation.x + Random.Range(-shake_intensity, shake_intensity) * .2f,
+            originRotation.y + Random.Range(-shake_intensity, shake_intensity) * .2f,
+            originRotation.z + Random.Range(-shake_intensity, shake_intensity) * .2f,
+            originRotation.w + Random.Range(-shake_intensity, shake_intensity) * .2f);*/
+            shake_intensity -= shake_decay;
+        }
+
+        else
+        {
+           // Debug.Log("stopped shaking");
+            //shaking = false;
+            transform.localPosition = originPosition;
+            //_transform.localRotation = originRotation;
+        }
+    
+      // if(transform.localPosition.y>0.02 || transform.localPosition.y<-0.02)
+       //   {
+     //  transform.localPosition.y=0;
+     //  }
+    
+      
+    }
+
+    void Shake()
+    {
+        originPosition = transform.localPosition;
+       // originRotation = transform.rotation;
+        //shake_intensity = .03f;
+      // shake_decay = 0.002f;
+    }
+}
+
