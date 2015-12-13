3e4d47b88f3dab908d6fa63560c6def2cd29ba76
Molocai <molocai@molocai.com>
2015-12-13 23:34:40 +0100

    yolo

--- Assets/Scripts/cameraShake.cs
+++ Assets/Scripts/cameraShake.cs
@@ -1,69 +1,4 @@
-﻿/*using UnityEngine;
-using System.Collections;
-public class cameraShake : MonoBehaviour
-{
-    private Vector3 originPosition;
-    private Quaternion originRotation;
-    public float shake_decay;
-    public float shake_intensity;
-    private Transform _transform;
-    /*void OnGUI()
-    {
-        if (GUI.Button(new Rect(20, 40, 80, 20), "Shake"))
-        {
-            Shake();
-        }
-    }
-
-       
-
-    void Update()
-    {
-
-        Shake();
-
-        if (shake_intensity > 0)
-        {
-            transform.localPosition = originPosition + Random.insideUnitSphere * shake_intensity;
-           /* transform.rotation = new Quaternion(
-            originRotation.x + Random.Range(-shake_intensity, shake_intensity) * .2f,
-            originRotation.y + Random.Range(-shake_intensity, shake_intensity) * .2f,
-            originRotation.z + Random.Range(-shake_intensity, shake_intensity) * .2f,
-            originRotation.w + Random.Range(-shake_intensity, shake_intensity) * .2f);
-            shake_intensity -= shake_decay;
-        }
-
-        else
-        {
-           // Debug.Log("stopped shaking");
-            //shaking = false;
-            transform.localPosition = originPosition;
-            //_transform.localRotation = originRotation;
-        }
-    
-      // if(transform.localPosition.y>0.02 || transform.localPosition.y<-0.02)
-       //   {
-     //  transform.localPosition.y=0;
-     //  }
-    
-      
-    }
-
-    void Shake()
-    {
-        originPosition = transform.localPosition;
-       // originRotation = transform.rotation;
-        //shake_intensity = .03f;
-      // shake_decay = 0.002f;
-    }
-}*/
-
-
-
-
-
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class cameraShake : MonoBehaviour
@@ -78,8 +13,6 @@ public class cameraShake : MonoBehaviour
     // Amplitude of the shake. A larger value shakes the camera harder.
     public float shakeAmount = 0.7f;
     public float decreaseFactor = 1.0f;
-
-    public float decreaseAmount;
 
     Vector3 originalPos;
 
@@ -98,10 +31,6 @@ public class cameraShake : MonoBehaviour
 
     void Update()
     {
-
-        while (shake >= 0)
-            shake -= decreaseAmount;
-
         if (shake > 0)
         {
             camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
