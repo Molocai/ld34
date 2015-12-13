ce179e830cb1624933d4872c5ffe5473088d8d1b
Molocai <molocai@molocai.com>
2015-12-13 16:25:55 +0100

    Interface

---
+++ Assets/Scripts/MainMenu/YouLooseMenuHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class YouLooseMenuHandler : MonoBehaviour {
+
+    public string MainMenuToLoad;
+    public string TryAgainToLoad;
+
+    public void PlayMenu()
+    {
+        SceneManager.LoadScene(MainMenuToLoad);
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(TryAgainToLoad);
+
+    }
+}
