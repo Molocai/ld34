8f6f3ecdc7763f083ae23f8b482107ea3861ee81
Molocai <molocai@molocai.com>
2015-12-14 22:56:04 +0100

    LAST COMMIT

--- Assets/Scripts/EndScreenController.cs
+++ Assets/Scripts/EndScreenController.cs
@@ -16,36 +16,24 @@ public class EndScreenController : MonoBehaviour {
     private float activateTime;
     private AudioSource[] audios;
     private AudioSource mAudio;
-    private bool isStarting = true;
 
     // Use this for initialization
     void OnEnable () {
         activateTime = Time.time;
 
         if (type == ENDSCREENTYPE.WIN)
-        {
             Destroy(PlayerController.Get.gameObject);
-
-            //AudioSource[] audios = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
-            //foreach (AudioSource aud in audios)
-            //    aud.volume = 0.0f;
-            //AudioSource mAudio = GetComponent<AudioSource>();
-            //mAudio.volume = 1.0f;
-        }
     }
 	
-	// Update is called once per frame
 	void Update () {
 
-        if (type == ENDSCREENTYPE.WIN && isStarting)
+        if (type == ENDSCREENTYPE.WIN)
         {
             audios = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
             foreach (AudioSource aud in audios)
                 aud.volume = Mathf.Lerp(aud.volume, 0.0f, Time.deltaTime * 0.8f);
             mAudio = GetComponent<AudioSource>();
             mAudio.volume = 1.0f;
-
-            isStarting = false;
         }
 
         if (Time.time >= activateTime + timeBeforeActivation)
