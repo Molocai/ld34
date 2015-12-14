31f6b0e505401d1804736425ab2743f1e54bd4ea
Molocai <molocai@molocai.com>
2015-12-14 01:56:07 +0100

    player die when hit by nova

--- Assets/Scripts/NovaController.cs
+++ Assets/Scripts/NovaController.cs
@@ -1,22 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NovaController : MonoBehaviour {
+public class NovaController : MonoBehaviour
+{
 
     public float initialMoveSpeed;
     public float speedboostFrequency;
     public float boostForce;
+    public AudioClip audioPlayerDestruction;
 
     private Rigidbody2D body;
+    private AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         body = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
 
         body.AddForce(Vector2.right * initialMoveSpeed);
 
         StartCoroutine(PushNovaABit(speedboostFrequency));
-	}
+    }
 
     IEnumerator PushNovaABit(float waitTime)
     {
@@ -24,6 +29,15 @@ public class NovaController : MonoBehaviour {
         {
             body.AddForce(Vector2.right * boostForce);
             yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            audioSource.PlayOneShot(audioPlayerDestruction);
+            // TODO: Die
         }
     }
 }
