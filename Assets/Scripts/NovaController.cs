a304f81a4b3dfc89e0ac61a27d61b98f8278b803
Molocai <molocai@molocai.com>
2015-12-14 01:13:30 +0100

    NovaController.cs

---
+++ Assets/Scripts/NovaController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NovaController : MonoBehaviour {
+
+    public float initialMoveSpeed;
+    public float speedboostFrequency;
+    public float boostForce;
+
+    private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+
+        body.AddForce(Vector2.right * initialMoveSpeed);
+
+        StartCoroutine(PushNovaABit(speedboostFrequency));
+	}
+
+    IEnumerator PushNovaABit(float waitTime)
+    {
+        while (true)
+        {
+            body.AddForce(Vector2.right * boostForce);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
